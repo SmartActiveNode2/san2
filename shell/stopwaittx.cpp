1c081c3e2d070355218b628bfc78382c9e017c3a
Pavel Slechta <slechta@email.cz>
2013-06-15 15:11:32 +0200

    Buffer trim fix (remove 4B ports form begining of response)

--- shell/stopwaittx.cpp
+++ shell/stopwaittx.cpp
@@ -1,13 +1,16 @@
 
 #include "stopwaittx.hpp"
+#include "utils/cdatapack.hpp"
 
 StopWaitTx::StopWaitTx(unsigned int repetitions, unsigned int timeout, San2::Api::CNodeConnector &connector, San2::Network::SanAddress serverAddress, SAN_UINT16 serverPort, San2::Network::SanAddress clientAddress, SAN_UINT16 clientPort) :
-	StopWait(repetitions, timeout),
+	m_repetitions(repetitions),
+	m_timeout(timeout),
 	m_connector(connector),
 	m_serverAddress(serverAddress),
 	m_serverPort(serverPort),
 	m_clientAddress(clientAddress),
-	m_clientPort(clientPort)
+	m_clientPort(clientPort),
+	m_expectedSeqNum(1)
 {
 	
 }
@@ -26,8 +29,92 @@ bool StopWaitTx::sendDatagram(San2::Utils::bytes& data)
 
 bool StopWaitTx::awaitDatagram(San2::Utils::bytes& data, unsigned int maxWaitMsec)
 {
+	SAN_UINT16 dstport, srcport;
 	San2::Network::CCapsule capsule;
 	if (SAN2_WAITFORCAPSULE_SUCCESS != m_connector.waitForCapsule(capsule, maxWaitMsec)) return false;
+	
+	if (capsule.getPortsDS(dstport, srcport) == false)
+	{
+		printf(" StopWaitTx::awaitDatagram(): getPortsDS - parse false\n");
+		return false;
+	}
+	
+	if (dstport != m_clientPort)
+	{
+		printf("Client port missmatch\n");
+		return false;
+	}
+	
+	if (srcport != m_serverPort)
+	{
+		printf("Server port missmatch\n");
+		return false;
+	}
+	
+	if (capsule.getSourceAddress() != m_serverAddress)
+	{
+		printf("Server address missmatch");
+		return false;
+	}
+	
+	
 	data = capsule.getData();
+	data.erase(data.begin(), data.begin() + 4); // remove ports
+	
 	return true;
+}
+
+
+bool StopWaitTx::sendReliableMessage(const San2::Utils::bytes& request, San2::Utils::bytes& response)
+{
+	response.clear();
+	
+	// Form the stop & wait packet format
+	
+	// SOF: ASCII "S"
+	// SEQ: UINT64 . Starting with one!
+	// PAYLOAD
+	San2::Utils::bytes data;
+	unsigned char sof = 0x53;
+	data += sof;
+	data += San2::Utils::CDataPack::pack(m_expectedSeqNum);
+	data += request;
+	
+	unsigned int tries = m_repetitions;
+	
+	while(tries > 0)
+	{
+		if (sendDatagram(data) == false)
+		{
+			printf("StopWait::sendDatagram failed\n");
+			return false;
+		}
+		
+		if (awaitDatagram(response, m_timeout) == false)
+		{
+			tries--;
+			printf("StopWait: Datagram lost; resending\n");
+			continue;
+		} 
+		
+		// Check structure of response
+		if (response.size() < 9) return false;
+		if (response[0] != 0x53) return false;
+		
+		SAN_UINT64 rxseq = San2::Utils::CDataPack::unpackUint64(response, 1);
+		
+		if (rxseq != m_expectedSeqNum)
+		{
+			tries--;
+			printf("StopWait: incomming sequence number missmatch\n");
+			continue;
+		}
+		
+		// success
+		response.erase(response.begin(), response.begin() + 9); // trim header
+		m_expectedSeqNum++;
+		return true;
+	}
+	
+	return false;
 }
--- shell/stopwaittx.hpp
+++ shell/stopwaittx.hpp
@@ -3,20 +3,24 @@
 
 #include "utils/cvector.hpp"
 #include "utils/platform/basictypes.hpp"
-#include "stopwait.hpp"
+#include "api/node/connector/cnodeconnector.hpp"
 
-class StopWaitTx : public StopWait
+class StopWaitTx
 {
 public:
 	StopWaitTx(unsigned int repetitions, unsigned int timeout, San2::Api::CNodeConnector &connector, San2::Network::SanAddress dstAddress, SAN_UINT16 dstPort, San2::Network::SanAddress srcAddress, SAN_UINT16 srcPort);
+	bool sendReliableMessage(const San2::Utils::bytes& request, San2::Utils::bytes& response);
 	
+private:
 	bool sendDatagram(San2::Utils::bytes& data);
 	bool awaitDatagram(San2::Utils::bytes& data, unsigned int maxWaitMsec);
 
-private:
+	unsigned int m_repetitions;
+	unsigned int m_timeout;
 	San2::Api::CNodeConnector &m_connector;
 	San2::Network::SanAddress m_serverAddress;
 	SAN_UINT16 m_serverPort;
 	San2::Network::SanAddress m_clientAddress;
 	SAN_UINT16 m_clientPort;
+	SAN_UINT64 m_expectedSeqNum;
 };
